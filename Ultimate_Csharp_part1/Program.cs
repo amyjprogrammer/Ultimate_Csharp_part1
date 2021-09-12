eec3e1aeb35a7162bab0e0559584f8731c0309c9
amyjprogrammer <amyjprogrammer@gmail.com>
2021-09-11 20:26:39 -0400

    Add project files.

---
+++ Ultimate_Csharp_part1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ultimate_Csharp_part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
