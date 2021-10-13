18e1e72a3df13ef03bb4417151669c297ef435a3
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-13 00:10:03 -0400

    directories and path

--- Files_Directories/File_Dir.cs
+++ Files_Directories/File_Dir.cs
@@ -11,7 +11,7 @@ namespace Files_Directories
     {
         static void Main(string[] args)
         {
-            //static methods- faster call
+            /*//static methods- faster call
             var path = @"c:\somefile.jpg";
             File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
             File.Delete(path);
@@ -29,7 +29,32 @@ namespace Files_Directories
             if (fileInfo.Exists)
             {
                 //code if the file exists
-            }
+            }*/
+
+            //Directories
+            /*Directory.CreateDirectory(@"c:\temp\folder1");
+            var files = Directory.GetFiles(@"c:\EFAProjects", "*.sln", SearchOption.AllDirectories); //gets an array of strings, with * looking for everything and .sln 
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }*/
+
+            /*var directories = Directory.GetDirectories(@"c:\EFAProjects\CSharp_Fundamentals", "*.*", SearchOption.AllDirectories);
+            foreach(var dir in directories)
+                Console.WriteLine(dir);//this works on computer
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();*/
+
+            //path
+            var path = @"C:\EFAProjects\DevTeam\DevTeam.sln";
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+            Console.WriteLine("File Name w/out extension: " + Path.GetFileNameWithoutExtension(path));
+
         }
     }
 }
